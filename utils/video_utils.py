f5d5dc8910d0ae0b673915af5ec8205d3b5574c8
R Chinmay <chinmaymighty@gmail.com>
2022-03-09 01:34:51 +0530

    Update video_utils.py

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -8,19 +8,16 @@ def create_video_from_intermediate_results(results_path, img_format):
     # change this depending on what you want to accomplish (modify out video name, change fps and trim video)
     #
     out_file_name = 'out.mp4'
-    fps = 30
-    first_frame = 0
-    number_of_frames_to_process = len(os.listdir(results_path))  # default don't trim take process every frame
+    fps = 3
 
     ffmpeg = 'ffmpeg'
     if shutil.which(ffmpeg):  # if ffmpeg is in system path
-        img_name_format = '%' + str(img_format[0]) + 'd' + img_format[1]  # example: '%4d.png' for (4, '.png')
+        img_name_format = '*' + img_format[1]  # example: '%4d.png' for (4, '.png')
         pattern = os.path.join(results_path, img_name_format)
         out_video_path = os.path.join(results_path, out_file_name)
 
-        trim_video_command = ['-start_number', str(first_frame), '-vframes', str(number_of_frames_to_process)]
-        input_options = ['-r', str(fps), '-i', pattern]
+        input_options = ['-r', str(fps), '-pattern_type', 'glob', '-i', str(pattern)]
         encoding_options = ['-c:v', 'libx264', '-crf', '25', '-pix_fmt', 'yuv420p']
-        subprocess.call([ffmpeg, *input_options, *trim_video_command, *encoding_options, out_video_path])
+        subprocess.call([ffmpeg, *input_options, *encoding_options, out_video_path])
     else:
         print(f'{ffmpeg} not found in the system path, aborting.')
